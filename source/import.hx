c79be6a94f77bae0ba94287026bfd9f54ccac6cf
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-04-28 07:19:37 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -33,5 +33,24 @@ import funkin.states.*;
 import funkin.objects.BGSprite;
 import funkin.backend.MusicBeatState;
 
+//Mobile Controls
+import mobile.flixel.FlxHitbox;
+import mobile.flixel.FlxVirtualPad;
+import mobile.flixel.input.FlxMobileInputID;
+import mobile.backend.SUtil;
+  
+//Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
 using StringTools;
 #end
