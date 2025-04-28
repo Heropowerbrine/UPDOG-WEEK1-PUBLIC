fe98844b33247da33bcee6cfba189382ac40c999
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-04-28 18:40:21 +0800

    Update CharacterEditorState.hx

--- source/funkin/states/editors/CharacterEditorState.hx
+++ source/funkin/states/editors/CharacterEditorState.hx
@@ -27,7 +27,7 @@ import openfl.events.Event;
 import openfl.events.IOErrorEvent;
 import haxe.Json;
 import funkin.objects.Character;
-import flixel.system.debug.interaction.tools.Pointer;
+import flixel.system.debug.interaction.tools.Pointer.GraphicCursorCross;
 import lime.system.Clipboard;
 import flixel.animation.FlxAnimation;
 import funkin.objects.*;
