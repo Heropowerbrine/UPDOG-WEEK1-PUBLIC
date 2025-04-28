421b1e0f1ca2743c4c4dd6f3b43672738218a954
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-04-28 08:28:00 +0800

    Update CopyState.hx

--- source/mobile/states/CopyState.hx
+++ source/mobile/states/CopyState.hx
@@ -62,7 +62,7 @@ class CopyState extends MusicBeatState
 		checkExistingFiles();
 		if (maxLoopTimes <= 0)
 		{
-			MusicBeatState.switchState(new Splash());
+			FlxG.switchState(new Splash());
 			return;
 		}
 
@@ -119,7 +119,7 @@ class CopyState extends MusicBeatState
 				
 				FlxG.sound.play(Paths.sound('confirmMenu')).onComplete = () ->
 				{
-					MusicBeatState.switchState(new Splash());
+					FlxG.switchState(new Splash());
 				};
 		
 				canUpdate = false;
